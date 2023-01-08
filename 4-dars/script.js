c52a1a71575e9e7f5a97a8e43fdb4f3a8d47da8f
shaxa_9505 <87860218+shaxa9505@users.noreply.github.com>
2023-01-08 21:04:26 +0500

    Add files via upload

---
+++ 4-dars/script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// alert("Hello javaScript");
+
+// const place = confirm("Are you here?"); // berga true yoki false keladi 
+// console.log(place);
+
+
+// const currentPlace = prompt("Where are you?", "Samar");
+// console.log(currentPlace);
+
+// console.log(typeof currentPlace); // data typeni tekwiriw
+
+
+// const age = prompt("How old are you", "18");
+// console.log(+age + 10);
+// console.log(typeof +age);
+
+
+const favouriteColor = [];
+favouriteColor[0] = prompt("What's your favourite color number #1", "");
+favouriteColor[1] = prompt("What's your favourite color number #2", "");
+favouriteColor[2] = prompt("What's your favourite color number #3", "");
+console.log(favouriteColor);
